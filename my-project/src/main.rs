4d87a4d523fbc8a1e155bb32043da361949ee064
Takeru Otsu <takeru.o.2002@gmail.com>
2023-04-21 18:09:32 +0900

    complete

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -2,33 +2,41 @@ use std::time::{Instant};
 
 fn main() {
     let start =Instant::now();
-    let mut a:u32=3;
+    let mut a:u32=2;
     //let mut count:usize = 1;
     //let mut array :[u32;10000] = [0;10000];
-    let mut v0: Vec<u32> = Vec::new();
-    v0.push(2);
+    //let mut v0: Vec<u32> = Vec::new();
+    
+    
     loop {
-        let mut t:u32=3;
+        let mut t:u32;
+        if a>2 {t=3;}
+        else {t=2;} 
+        
         loop {
             if a%t==0 {
                 break;
             }
-            t+=2;   
+            if a>2 {t+=2}
+            else {t+=1} 
         };
         //if result==t { println!("{}",result) };
         if a==t { 
-            v0.push(a);
+            println!("{}",a);
         };
         
-        a+=2;
-
+        if a>2 {a+=2}
+        else {a+=1}
+        
+        
         if a>u32::max_value() { break }
     }
-    let arr_str = v0.iter()
+    
+    /*let arr_str = v0.iter()
                      .map(|&x| x.to_string())
                      .collect::<Vec<String>>()
                      .join("\n");
-    println!("{}", arr_str);
+    println!("{}", arr_str);*/
     let end =start.elapsed();
     println!("{}.{:000000005}s", end.as_secs(), end.subsec_nanos() / 1_000_000);
 }
