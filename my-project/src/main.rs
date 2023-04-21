e1ec8b1675c8ae18e95f109352e9cae358a2c942
Takeru Otsu <takeru.o.2002@gmail.com>
2023-04-21 09:33:30 +0900

    edit

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,4 +1,17 @@
 fn main() {
     let mut a:u32=3;
     println!("2");
+    loop {
+        let mut t:u32=3;
+        let result:u32=loop {
+            if a%t==0 {
+                break a;
+            }
+            t+=2;   
+        };
+        if result==t { println!("{}",result) };
+        a+=2;
+
+        if a>u32::pow(2,31) { break }
+    }
 }
